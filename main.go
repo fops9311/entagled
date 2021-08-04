f4a1886e62722aeb2417261d3ae5479713d1e382
fops9311@yandex.ru <fops9311@yandex.ru>
2021-08-04 08:08:27 +0300

    more general w interface

--- main.go
+++ main.go
@@ -4,17 +4,17 @@ import "fmt"
 
 func main() {
 	in, out := entagle()
-	in(true)
+	in(1)
 	fmt.Println(out())
 	in(false)
 	fmt.Println(out())
 }
-func entagle() (in func(bool), out func() bool) {
-	var data bool
-	in = func(d bool) {
+func entagle() (in func(interface{}), out func() interface{}) {
+	var data interface{}
+	in = func(d interface{}) {
 		data = d
 	}
-	out = func() bool {
+	out = func() interface{} {
 		return data
 	}
 	return in, out
